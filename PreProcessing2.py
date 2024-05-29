aebf5b85314a843d2896de229f185e39e3686884
ejsong25 <ejsong25@gmail.com>
2024-05-30 02:32:03 +0900

    전세 지역점수 추가

--- PreProcessing2.py
+++ PreProcessing2.py
@@ -13,7 +13,7 @@ plt.rcParams["axes.unicode_minus"] = False
 target(deposit(보증금)) 에 각 feaeture 가 미치는 영향 확인
 """
 df = pd.read_csv("jeonse_dataset.csv")
-
+pd.set_option("display.max_seq_items", None)
 
 # 한글 칼럼명을 영어 칼럼명으로 변환
 def convert_to_english(dataframe):
@@ -44,8 +44,6 @@ def convert_to_english(dataframe):
         "Taepyeong",
         "Hadaewon",
     ]
-
-    # 한글 칼럼명을 영어 칼럼명으로 변환
     dataframe.columns = [
         english_columns.pop(0) if col.endswith("동") else col
         for col in dataframe.columns
@@ -64,6 +62,7 @@ df["building_age"] = scaler.fit_transform(df[["building_age"]])
 X = df.iloc[:, 0:6]  # 'Road_condition' ~ 'Construction_year' columns
 y = df.iloc[:, -1]  # 'Deposit' column
 
+# 동 제외
 """
 Importance scoring, corr heatmap 등을 제작할 때,
 모든 "동" 을 포함하기보단 "동"은 제외하고 다른 특성들만 우선 적용
@@ -76,6 +75,8 @@ model.fit(X, y)
 print(f"[전세 데이터]: Feature Importance (동 컬럼 제외): {model.feature_importances_}")
 
 feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+print(f"[전세 데이터]: Feature Importance (동 컬럼 제외): {feat_importances}")
+
 plt.figure(figsize=(10, 6))
 plt.title("[전세 데이터]: Feature Importance 시각화 (동 컬럼 제외)")
 feat_importances.nlargest(6).plot(kind="barh")
@@ -89,12 +90,57 @@ plt.title("[전세 데이터]: Correlation Matrix 시각화 (동 컬럼 제외)"
 g = sns.heatmap(corrmat, annot=True, cmap="RdYlGn")
 plt.show()
 
+# Only 동
 """
 모든 "동" 특성들을 이용하여 상관관계 파악. 어느 동이 평균적으로 비싼지 등 판단 가능
 """
 df_only_distrcit = df.drop(X.columns, axis=1)
-
 corrmat = df_only_distrcit.corr()
+
+# ----------------------------------------------------------------------
+# "동" 컬럼만 추출하여 지역 점수 생성
+'''
+'동'과 보증금 간의 상관관계 계산, 
+보증금 - 보증금 상관관계는 1이므로 drop
+상관관계를 표준화 하여 지역 점수로 반영
+'''
+# 각 '동'이 보증금에 미치는 상관 관계 계산, 표준화 => 지역점수
+district_corr = df_only_distrcit.corr()["deposit"].drop("deposit")
+district_index = district_corr.index  # 지역명 인덱스 저장
+
+district_corr = district_corr.values.reshape(-1, 1)
+district_scores = StandardScaler().fit_transform(district_corr)
+
+# column : 동, row: district score
+district_scores_df = pd.DataFrame(district_scores, index=district_index, columns=["District Score"]).T
+print(district_scores_df)
+
+
+# 지역 점수를 원래 데이터프레임에 추가
+# 각 행에서 True인 값의 컬럼명을 찾아 해당하는 지역 점수를 새 컬럼으로 추가
+
+def get_district_score(row):
+    for col in district_index:
+        if row[col] == 1:
+            return district_scores_df[col].values[0]
+    return 0  # 모든 값이 False인 경우
+
+df["District Score"] = df.apply(get_district_score, axis=1)
+
+# 원핫 인코딩된 컬럼 drop
+df = df.drop(columns=district_index)
+deposit = df.pop("deposit")
+df['deposit'] = deposit
+print(df.head())
+
+# 지역 점수를 포함한 상관관계 히트맵 생성
+corrmat_district_score = df[["District Score", "deposit"]].corr()
+plt.figure(figsize=(6, 6))
+plt.title("[전세 데이터]: Correlation Matrix 시각화 (지역 점수 포함)")
+sns.heatmap(corrmat_district_score, annot=True, cmap="RdYlGn")
+plt.show()
+# ---------------------------------------------------------------------
+
 
 top_corr_features = corrmat.index
 plt.figure(figsize=(15, 15))
@@ -105,6 +151,7 @@ plt.show()
 # normalize된 전세 데이터를 csv로 저장
 # df.to_csv('jeonse_dataset_normalized.csv', index=False, encoding='utf-8-sig')
 
+# ---------------------------------------------------------------------------------------------------------
 
 """
 월세 데이터셋을 사용.
@@ -124,7 +171,7 @@ df["monthly_rent_bill"] = scaler.fit_transform(df[["monthly_rent_bill"]])
 X = df.iloc[:, 0:7]  # 'Road_condition' ~ 'Contract_month' columns
 y = df.iloc[:, -1]  # 'Monthly_rent_bill' column
 
-
+# 동 제외
 """
 Importance scoring, corr heatmap 등을 제작할 때
 모든 "동" 을 포함하기보단, "동"은 제외하고 다른 특성들만 우선 적용
@@ -149,6 +196,7 @@ plt.title("[월세 데이터]: Correlation Matrix 시각화 (동 컬럼 제외)"
 g = sns.heatmap(corrmat, annot=True, cmap="RdYlGn")
 plt.show()
 
+# only 동
 """
 모든 "동" 특성들을 이용하여 상관관계 파악. 어느 동이 평균적으로 비싼지 등 판단 가능
 """
