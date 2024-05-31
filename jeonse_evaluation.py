147c401aa91aafca01185427b1d1d9b46975fc4c
SANGWOO PARK <swp1017@naver.com>
2024-06-01 03:53:40 +0900

    evaluation

--- jeonse_evaluation.py
+++ jeonse_evaluation.py
@@ -3,7 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import (train_test_split,
+                                     KFold,
+                                     cross_val_score,
+                                     RepeatedStratifiedKFold,
+                                     RepeatedKFold,
+                                     StratifiedKFold)
 from sklearn.metrics import (
     mean_absolute_error,
     r2_score,
@@ -46,7 +51,7 @@ y_pred = model.predict(X_test)
 # RMSE, R2 Score 계산
 mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
-print(f"Mean_Absolute_Error: {mae}, R2_Score: {r2}\n")
+
 
 # 예측 결과 시각화
 plt.scatter(y_test, y_pred, s=0.5)
@@ -54,8 +59,31 @@ plt.plot([y.min(), y.max()], [y.min(), y.max()], "r-", lw=1)
 plt.xlabel("Actual Deposit")
 plt.ylabel("Predicted Deposit")
 plt.title("Linear Regression Plot")
-#plt.show()
+plt.show()
 
+# 10-Fold Cross Validation 
+kfold = KFold(n_splits=10, shuffle=True, random_state=42)
+
+# Repeated 10-Fold Cross Validation
+repeated_kfold = RepeatedKFold(n_splits=10, n_repeats=10, random_state=42)
+
+# CVS 계산
+cvs = cross_val_score(model, X, y, cv=kfold)
+repeated_cvs = cross_val_score(model, X, y, cv=repeated_kfold, scoring='r2')
+
+# 전세 데이터셋 Linear Regression Model Evaluation
+print("<Jeonse dataset Linear Regression Model Evaluation>")
+print(f"1.Mean_Absolute_Error: {mae}, R2_Score: {r2}\n")
+
+# CVS 값과 CVS 평균값 비교
+print("CVS Value")
+print("(1)KFold CVS\n", cvs)
+print()
+print("(2)Repeated KFold CVS\n", repeated_cvs)
+print()
+print("2.CVS Mean Comparison")
+print(f"CVS Mean: {cvs.mean()}")
+print(f"Repeated KFold CVS Mean: {repeated_cvs.mean()}\n\n")
 
 """ classification (전세 보증금을 특정 기준에 따라 분류) """
 
@@ -126,10 +154,52 @@ precision = precision_score(y_test, y_pred, average="weighted")
 recall = recall_score(y_test, y_pred, average="weighted")
 f1_ = f1_score(y_test, y_pred, average="weighted")
 
+# 10-Fold Cross Validation
+kfold = KFold(n_splits=10, shuffle=True, random_state=42)
+
+# Stratified 10-Fold Cross Validation
+stratified_kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+
+# Repeated 10-Fold Cross Validation
+repeated_kfold = RepeatedKFold(n_splits=10, n_repeats=10, random_state=42)
+
+# Repeated Stratified 10-Fold Cross Validation
+repeated_stratified_kfold = RepeatedStratifiedKFold(n_splits=10, n_repeats=10, random_state=42)
+
+# CVS 계산
+cvs = cross_val_score(model, X, y, cv=kfold)
+stratified_cvs = cross_val_score(model, X, y, cv=stratified_kfold, scoring='accuracy')
+repeated_cvs = cross_val_score(model, X, y, cv=repeated_kfold, scoring='accuracy')
+repeated_stratified_cvs = cross_val_score(model, X, y, cv=repeated_stratified_kfold, scoring='accuracy')
+
+# 각 CVS 값과 CVS 평균값 비교
+print("CVS Value")
+print("(1)KFold CVS\n", cvs)
+print()
+print("(2)Stratified KFold CVS\n", stratified_cvs)
+print()
+print("(3)Repeated KFold CVS\n", repeated_cvs)
+print()
+print("(4)Repeated Stratified KFold CVS\n", repeated_stratified_cvs)
+print()
+
+# 전세 데이터셋 Classification Model Evaluation
+print("<Jeonse dataset Classification Model Evaluation>")
+
+# Show Accuracy, Precision, Recall, F1 Score
+print("1.Accuracy, Precision, Recall, F1 Score") 
 print(f"Accuracy: {accuracy}")
 print(f"Precision: {precision}")
 print(f"Recall: {recall}")
-print(f"F1 Score: {f1_}")
+print(f"F1 Score: {f1_}\n")
+
+# 각 KFold 메서드 CSV 평균값 비교
+print("2.KFold CVS Mean Comparison")
+print(f"KFold CVS Mean: {cvs.mean()}\n"
+      f"Stratified KFold CVS Mean: {stratified_cvs.mean()}\n"
+      f"Repeated KFold CVS Mean: {repeated_cvs.mean()}\n"
+      f"Repeated Stratified KFold CVS Mean: {repeated_stratified_cvs.mean()}\n"
+      )
 
 # Confusion Matrix 시각화
 plt.figure(figsize=(10, 6))
@@ -144,9 +214,4 @@ sns.heatmap(
 plt.xlabel("Actual")
 plt.ylabel("Predicted")
 plt.title("Jeonse Deposit Confusion Matrix")
-#plt.show()
-
-class_cnt =jeonse_data["deposit_category"].value_counts()
-total_samples = len(jeonse_data)
-class_ratio = class_cnt / total_samples
-print(class_ratio)
+plt.show()
